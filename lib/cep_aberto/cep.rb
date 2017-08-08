b72111567ed8a21ca0ac9fd518a4869300614d07
Jefferson Felix <jsfelix@gmail.com>
2017-08-08 18:57:03 +0000

    Version 0.1.0: added cities search functionality

--- lib/cep_aberto/cep.rb
+++ lib/cep_aberto/cep.rb
@@ -9,39 +9,60 @@ module CepAberto
     require "uri"
     require "json"
     
-    @domain = "http://www.cepaberto.com/api/v2/ceps.json"
+    # CEP Aberto API (v2)
+    @domain = "http://www.cepaberto.com/api/v2/"
 
     # Find an address from a zipcode using the CEP Aberto API
     # Params:
     # +zipcode+:: the zipcode to be searched by CEP Aberto API
     # +token+:: the user authorization token
-    # Return: Hash with the full address
+    # Returns a hash with the full address
     def Cep::find(cep, token)
       validate_cep(cep)
-      uri = URI(@domain)
+      uri = URI("#{@domain}ceps.json")
       params = {'cep' => cep}
-      headers = { "Authorization" => "Token token=#{token}" }
-      
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new(uri.path)
-      request.set_form_data(params)
-      request = Net::HTTP::Get.new(uri.path + '?' + request.body, headers)
-      response = http.request(request).body
-      address = JSON.parse(response)
+      address = request_cep(uri, params, token)
       if(address["logradouro"].is_a?String)
         address["logradouro"] = address["logradouro"].split(",")[0]
       end
       return address
     end
 
+    # Returns a list of cities from a given state.
+    # Params:
+    # +state+:: State acronym (e.g. SP)
+    # +token+:: the user authorization token
+    # Returns an array with all cities from the state
+    def Cep::cities(state, token)
+      uri = URI("#{@domain}cities.json")
+      params = {'estado' => state}
+      all_cities = request_cep(uri, params, token)
+      cities_arr = Array.new
+      all_cities.each do |c|
+        cities_arr.push(c["nome"])
+      end
+      return cities_arr
+    end
+    
     private
     
+    # Validate cep entry
+    # * Must be a string
+    # * Must be 8 characters
     def Cep::validate_cep(str)
       raise ArgumentError, 'Argument is not string with 8 characters' unless str.is_a? String and str.length == 8
     end
+    
+    # Request API
+    def Cep::request_cep(uri, params, token)
+      headers = { "Authorization" => "Token token=#{token}" }
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.path)
+      request.set_form_data(params)
+      request = Net::HTTP::Get.new(uri.path + '?' + request.body, headers)
+      response = http.request(request).body
+      JSON.parse(response)
+    end
+    
   end
 end
-
-
-
-

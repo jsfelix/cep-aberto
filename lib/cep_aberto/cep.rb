2f98140f2de6f5c7ca4897628737b69a95fe86d9
Jefferson dos Santos Felix <jsfelix@gmail.com>
2017-08-11 15:11:16 -0300

    Excludes districts in method :cities()

--- lib/cep_aberto/cep.rb
+++ lib/cep_aberto/cep.rb
@@ -1,18 +1,18 @@
 # Main module CepAberto.
 # Author:: Jefferson Felix
 module CepAberto
-  # This class holds the methods to find zipcodes and addresses using the CEP 
+  # This class holds the methods to find zipcodes and addresses using the CEP
   # Aberto API.
   class Cep
-    
+
     require "net/http"
     require "uri"
     require "json"
-    
+
     # CEP Aberto API (v2)
     @domain = "http://www.cepaberto.com/api/v2/"
 
-    # Find an address from a zipcode using the CEP Aberto API.
+    # Find an address from a zipcode using the CEP Aberto API. <br />
     # Params:
     # +zipcode+:: the zipcode to be searched by CEP Aberto API
     # +token+:: the user authorization token
@@ -28,7 +28,7 @@ module CepAberto
       return address
     end
 
-    # Returns a list of cities from a given state.
+    # Returns a list of cities from a given state. This method excludes districts. <br />
     # Params:
     # +state+:: State acronym (e.g. SP)
     # +token+:: the user authorization token
@@ -39,20 +39,20 @@ module CepAberto
       all_cities = request_cep(uri, params, token)
       cities_arr = Array.new
       all_cities.each do |c|
-        cities_arr.push(c["nome"])
+        cities_arr.push(c["nome"]) unless c["nome"].end_with?(')')
       end
       return cities_arr
     end
-    
+
     private
-    
+
     # Validate cep entry
     # * Must be a string
     # * Must be 8 characters
     def Cep::validate_cep(str)
       raise ArgumentError, 'Argument is not string with 8 characters' unless str.is_a? String and str.length == 8
     end
-    
+
     # Request API
     def Cep::request_cep(uri, params, token)
       headers = { "Authorization" => "Token token=#{token}" }
@@ -63,6 +63,6 @@ module CepAberto
       response = http.request(request).body
       JSON.parse(response)
     end
-    
+
   end
 end
---
+++ lib/cep_aberto/teste.rb
@@ -0,0 +1,3 @@
+require_relative "cep.rb"
+
+puts CepAberto::Cep::cities("SP", "76b8f30c139629d8bdd35ee8744077ca")
